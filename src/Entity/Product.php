ec330646f3ead0f789170de609c8fba7443816c0
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-03-21 00:01:03 +0330

    Created an entity with interface

---
+++ src/Entity/Product.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Interface\EntityInterface;
+
+class Product implements EntityInterface
+{
+
+    public function __construct
+    (
+        private int    $id,
+        private string $title,
+        private string $description,
+        private string $category,
+        private float  $price
+    )
+    {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'title' => $this->getTitle(),
+            'description' => $this->getDescription(),
+            'category' => $this->getCategory(),
+            'price' => $this->getPrice()
+        ];
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param string $category
+     */
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+
+}
---
+++ src/Interface/EntityInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interface;
+
+interface EntityInterface
+{
+    public function toArray(): array;
+}
