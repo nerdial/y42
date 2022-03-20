d1e80178496271fcd313c05bb37d05401f6d7e07
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-03-21 00:49:51 +0330

    Fixed interface definition

--- src/Interface/WhereInterface.php
+++ src/Interface/WhereInterface.php
@@ -6,7 +6,7 @@ interface WhereInterface
 {
     public function limit(int $limit): self;
 
-    public function offset(int $offset = 1): self;
+    public function offset(int $offset = 0): self;
 
     public function delete(): bool;
 
